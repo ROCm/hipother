151e21ce9066904c9e7bcb5d288f5c5c8cb25e18
Shadi Dashmiz <94885391+shadidashmiz@users.noreply.github.com>
2026-02-27 22:25:27 +0000

    SWDEV-546305: Add hipKernelSetAttribute
    Signed-off-by: sdashmiz <shadi.dashmiz@amd.com>
    [rocm-systems] ROCm/rocm-systems#781 (commit 6adccf3)

--- hipnv/include/hip/nvidia_detail/nvidia_hip_runtime_api.h
+++ hipnv/include/hip/nvidia_detail/nvidia_hip_runtime_api.h
@@ -3787,6 +3787,13 @@ inline static hipError_t hipKernelGetParamInfo(hipKernel_t kernel, size_t paramI
                                                size_t* paramSize) {
   return hipCUResultTohipError(cuKernelGetParamInfo(kernel, paramIndex, paramOffset, paramSize));
 }
+inline static hipError_t hipKernelSetAttribute(hipFunction_attribute attrib, int value, hipKernel_t kernel, hipDevice_t dev) {
+  return hipCUResultTohipError(cuKernelSetAttribute(attrib, value, kernel, dev));
+}
+
+inline static hipError_t hipKernelGetFunction(hipFunction_t* pFunc, hipKernel_t kernel) {
+  return hipCUResultTohipError(cuKernelGetFunction(pFunc, kernel));
+}
 
 inline static hipError_t hipKernelGetAttribute(int* pi, hipFunction_attribute attrib, hipKernel_t kernel,
                                                hipDevice_t dev) {
