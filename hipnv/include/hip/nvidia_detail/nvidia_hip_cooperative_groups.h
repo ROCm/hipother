951793a50d7a1f2eb74c59dd9f21d33e41c5a9b8
Ethan Trinh <Ethan.Trinh@amd.com>
2026-03-17 13:50:39 +0000

    Update copyright for remaining files
    ## Motivation
    Update copyright to short form SPDX MIT License.
    <!-- Explain the purpose of this PR and the goals it aims to achieve.
    -->
    
    ## Technical Details
    Copyright updated in all owned hip, clr, hip-tests, hipother files.
    <!-- Explain the changes along with any relevant GitHub links. -->
    
    ## JIRA ID
    N/A
    <!-- If applicable, mention the JIRA ID resolved by this PR (Example:
    Resolves SWDEV-12345). -->
    <!-- Do not post any JIRA links here. -->
    
    ## Test Plan
    N/A
    <!-- Explain any relevant testing done to verify this PR. -->
    
    ## Test Result
    N/A
    
    <!-- Briefly summarize test outcomes. -->
    
    ## Submission Checklist
    
    - [x] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    [rocm-systems] ROCm/rocm-systems#4016 (commit dbf1223)

--- hipnv/include/hip/nvidia_detail/nvidia_hip_cooperative_groups.h
+++ hipnv/include/hip/nvidia_detail/nvidia_hip_cooperative_groups.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
 #ifndef HIP_INCLUDE_HIP_NVIDIA_DETAIL_HIP_COOPERATIVE_GROUPS_H
 #define HIP_INCLUDE_HIP_NVIDIA_DETAIL_HIP_COOPERATIVE_GROUPS_H
 
--- hipnv/include/hip/nvidia_detail/nvidia_hip_texture_types.h
+++ hipnv/include/hip/nvidia_detail/nvidia_hip_texture_types.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
 #ifndef HIP_INCLUDE_HIP_NVIDIA_DETAIL_HIP_TEXTURE_TYPES_H
 #define HIP_INCLUDE_HIP_NVIDIA_DETAIL_HIP_TEXTURE_TYPES_H
 
