741671a535f835f1f1b0e15ee7300c3ea7236eb4
Gerardo Hernandez <ghc.cpp@gmail.com>
2026-03-23 05:58:58 +0000

    ROCM-1254 - cooperative groups reduce
    * add implementation of cooperative_groups::reduce() API.
    * statically assert if a tile partition has the same size as the parent (AIRUNTIME-1876)
    * fix that warp reduce tests were always using the input data for the first warp, instead of all the generated warp-sized random input (which is of size kNumReduces)
    [rocm-systems] ROCm/rocm-systems#3784 (commit 1fe7989)

---
+++ hipnv/include/hip/nvidia_detail/nvidia_hip_cooperative_groups_reduce.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef HIP_INCLUDE_HIP_NVIDIA_DETAIL_HIP_COOPERATIVE_GROUPS_REDUCE_H
+#define HIP_INCLUDE_HIP_NVIDIA_DETAIL_HIP_COOPERATIVE_GROUPS_REDUCE_H
+
+#include <cooperative_groups/reduce.h>
+#include <cooperative_groups.h>
+#include <cuda_runtime.h>
+#include <hip/hip_runtime.h>
+#include <hip/hip_runtime_api.h>
+
+#endif // HIP_INCLUDE_HIP_NVIDIA_DETAIL_HIP_COOPERATIVE_GROUPS_REDUCE_H
